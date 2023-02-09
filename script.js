2e6b44e5b6f28c8f29c436c38268127a87406a35
Amer <aalshabebi@dineropay.com>
2023-02-09 15:12:37 +0300

    adding initial animation

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 /* fatimah's scripts */
 
 /* amer's scripts */
+
+let options = {
+	// root: document.querySelector(".payment_method"),
+	rootMargin: "0px",
+};
+let observer = new IntersectionObserver((entries) => {
+	let [image] = entries;
+	console.log(image);
+	if (!image.isIntersecting) {
+		image.target.style.transform = "translateY(500px)";
+	} else {
+		image.target.style.transform = "translateY(80px)";
+	}
+});
+const imageDashboard = document.querySelector(".dashboard__big--image");
+// const dashboard = document.querySelector(".dashboard");
+observer.observe(imageDashboard);
